df06ec1a90124b12b7ad26fd07817b0864ae3883
nelson <nelsonealves>
2021-04-09 19:00:25 -0300

    ...

---
+++ leitorDeFaturas/library.py
@@ -0,0 +1,20 @@
+grandezas = {
+
+}
+
+conventional = {
+    "te"
+    "tusd"
+}
+
+verde = {
+    'Consumo Ponta Tusd': 'tusdPonta',
+    'Consumo Ponta Te': 'tePonta',
+    'Consumo Fora Ponta Tusd': 'tusdForaPonta',
+    'Consumo Fora Ponta Tusd': 'teForaPonta',
+    #'Energia Injetada Fp Tusd': 
+    #'Energia Injetada Fp Te':
+    # 'Consumo Reat.Ponta Te':
+    # 'Consumo Reat.Fora Ponta Te':
+    'Demanda Tusd': 'demandaUnica'
+}
--- leitorDeFaturas/main.py
+++ leitorDeFaturas/main.py
@@ -2,19 +2,133 @@ import cv2
 import numpy as np
 import pytesseract
 import os
+import re
 
-# pytesseract.pytesseract.tesseract_cmd = "/home/nelson/Documents/leitorDeFaturas/leitorDeFaturas"
+pytesseract.pytesseract.tesseract_cmd = "/bin/tesseract"
 path = r"/home/nelson/Documents/leitorDeFaturas/leitorDeFaturas/img/Celesc/jpg/fatura.jpg"
-roi = [[(193*3, 104*3),(378*3, 184*3)]]
+
+# x1, y1, x2, y2
+x1 = 192*3
+y1 = 104*3
+x2 = 378*3
+y2 = 250*3
+
+roi = [[(x1, y1), (x2, y2)]]
 # roi = [[(193, 104),(378, 184)]]
 
 sourceImg = cv2.imread(path)
-h,w,c = sourceImg.shape
-sourceImg = cv2.resize(sourceImg, (w//1,h//1))
+h, w, c = sourceImg.shape
+# sourceImg = cv2.resize(sourceImg, (w//1, h//1))
 
 
-image = cv2.rectangle(sourceImg, roi[0][0], roi[0][1], (255,0,0), 2)
-imgCrop = sourceImg[193*3:104*3, 378*3: 184*3]
+imgMask = np.zeros_like(sourceImg)
 
-cv2.imshow('teste', imgCrop)
-cv2.waitKey(0)
+# image =
+imgShow = cv2.addWeighted(sourceImg, 0.99, imgMask, 0.1, 0)
+
+imgCrop = sourceImg[y1: y2, x1: x2]
+
+
+# cv2.imshow('teste', imgCrop)
+
+# Dados do Faturamento [x1, y1, x2, y2, color]
+pos1 = [
+    [1, 0, 292, 437, 'red'], [290, 0, 370, 437, 'red'], [
+        368, 0, 455, 437, 'red'], [482, 0, 554, 437, 'red'],
+]
+col1 = imgCrop[pos1[0][1]:pos1[0][3], pos1[0][0]:pos1[0][2]]
+col2 = imgCrop[pos1[1][1]:pos1[1][3], pos1[1][0]:pos1[1][2]]
+col3 = imgCrop[pos1[2][1]:pos1[2][3], pos1[2][0]:pos1[2][2]]
+col4 = imgCrop[pos1[3][1]:pos1[3][3], pos1[3][0]:pos1[3][2]]
+
+
+def trataImagem(img):
+    img = cv2.resize(img, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    kernel = np.ones((1, 1), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=1)
+    img = cv2.erode(img, kernel, iterations=1)
+    img = cv2.threshold(cv2.bilateralFilter(img, 5, 75, 75),
+                        0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+    return img
+
+
+col1Tratado = trataImagem(col1)
+col2Tratado = trataImagem(col2)
+col3Tratado = trataImagem(col3)
+col4Tratado = trataImagem(col4)
+
+
+# cv2.imshow('SEM RESIZE', col4)
+# cv2.imshow('com resize', col2Tratado)
+
+# cv2.waitKey(0)
+
+
+def separate(array, separator):
+    results = []
+    a = array[:]
+    i = 0
+    while i <= len(a)-len(separator):
+        if (a[i:i+len(separator)] == separator):
+
+            if a[:i] != '':
+               # print(f'Palavra: {a[:i]}')
+                # print('true')
+                results.append(a[:i])
+            a = a[i+len(separator):]
+            i = 0
+        else:
+            i += 1
+    results.append(a)
+    return results
+
+# col1Array = pytesseract.image_to_string(col1Tratado)
+# col2Array = pytesseract.image_to_string(col2Tratado)
+# col3Array = pytesseract.image_to_string(col3Tratado)
+# col4Array = pytesseract.image_to_string(col4Tratado)
+
+
+# col3Array = pytesseract.image_to_string(col3Resize)
+# col4Array = pytesseract.image_to_string(col4Resize)
+
+
+col1Array = separate(pytesseract.image_to_string(col1Tratado), '\n')
+col2Array = separate(pytesseract.image_to_string(col2Tratado), '\n')
+col3Array = separate(pytesseract.image_to_string(col3Tratado), '\n')
+col4Array = separate(pytesseract.image_to_string(col4Tratado), '\n')
+
+# i = 6
+# print (f'{col1Array}')
+# print (f'{col2Array}')
+# print (f'{col3Array}')
+# print (f'{col4Array}')
+
+
+# print (len(col1Array))
+# print (len(col2Array))
+# print (len(col3Array))
+# print (len(col4Array))
+
+arrayFinal = []
+
+for x in range(1, len(col1Array)):
+    # arrayFinal
+    if (col1Array[x][0:4] == 'Subt'):
+        break
+    else:
+        arrayFinal.append(col1Array[x])
+
+print(arrayFinal)
+
+# for x in range():
+#     arrayFinal.append([col1Array[i], col2Array[i], col3Array[i], col4Array[i]])
+
+# print (arrayFinal)
+# print(f'{image}')
+# print('valores')
+# print(f'{pytesseract.image_to_string(col4)}')
+
+
+# cv2.imshow('Dados faturamento', col1)
